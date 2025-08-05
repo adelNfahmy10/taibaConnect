13ed80caeb9f027c8c85b6e4a18bce7d4c50b997
AN-Fahmy <fahmyan@taibaconnect.com>
2025-08-05 16:59:02 +0300

    add links

--- src/app/feature/feature.component.ts
+++ src/app/feature/feature.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { SwiperOptions } from 'swiper/types';
 
 @Component({
   selector: 'app-feature',
   standalone: true,
   imports: [],
   templateUrl: './feature.component.html',
-  styleUrl: './feature.component.scss'
+  styleUrl: './feature.component.scss',
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class FeatureComponent {
-
+  swiperConfig: SwiperOptions = {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false
+    },
+    breakpoints: {
+      1200: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      0: {
+        slidesPerView: 1,
+      }
+    }
+  };
 }
